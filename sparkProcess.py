8ab0b86dfe1e0b3e20694010cdf93d259a4aa2c3
garywangop <garywangop@gmail.com>
2020-02-19 00:50:22 +0000

    Initialize spark processing app

--- sparkProcess.py
+++ sparkProcess.py
@@ -3,3 +3,11 @@ from pyspark.sql import Row,SQLContext
 from pyspark.streaming import StreamingContext
 import requests
 import sys
+
+conf = SparkConf()
+conf.setAppName("TwitterStreamApplication")
+sc = SparkContext(conf=conf)
+sc.setLogLevel("ERROR")
+ssc = StreamingContext(sc, 1)
+ssc.checkpoint("checkpoint_TwitterStreamApp")
+dataStream = ssc.socketTextStream("localhost",9090)
