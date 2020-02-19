4cf35793762ad7805a3a6fe8cffb0412584cc18e
garywangop <garywangop@gmail.com>
2020-02-19 00:53:09 +0000

    Use sumup_tags_counts function to sum up all counts for each word

--- sparkProcess.py
+++ sparkProcess.py
@@ -11,3 +11,6 @@ sc.setLogLevel("ERROR")
 ssc = StreamingContext(sc, 1)
 ssc.checkpoint("checkpoint_TwitterStreamApp")
 dataStream = ssc.socketTextStream("localhost",9090)
+
+def sumup_tags_counts(new_values, total_sum):
+    return (total_sum or 0) + sum(new_values)
